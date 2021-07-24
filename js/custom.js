fcf91ea490a145b62852ff31145d2222d5ab81c1
Apurbu Sarder <apurbusarderjust321@gmail.com>
2021-07-24 15:50:00 +0600

    Deal of the week section

--- js/custom.js
+++ js/custom.js
@@ -23,7 +23,7 @@ jQuery(document).ready(function($)
 
     initFavorite();
     initIsotopeFiltering();
-
+    initTimer();
 
 
 
@@ -32,7 +32,54 @@ jQuery(document).ready(function($)
 
 
 // 3. Init Timer
+function initTimer()
+{
+    if($('.timer').length)
+    {
 
+        // var target_date = new Date("July 31, 2021").getTime();
+
+        var date = new Date();
+        date.setDate(date.getDate() + 4);
+        var target_date = date.getTime();
+
+
+        // variables for time units
+
+        var days, hours, minutes, seconds;
+        var d = $('#day');
+        var h = $('#hour');
+        var m = $('#minute');
+        var s = $('#second');
+
+        setInterval(function ()
+        {
+            // find the amount of "seconds" between now and target
+
+            var current_date = new Date().getTime();
+            var seconds_left = (target_date - current_date) / 1000;
+
+            //do some time calculations
+
+            days = parseInt(seconds_left / 86400);
+            seconds_left = seconds_left % 86400;
+
+            hours = parseInt(seconds_left / 3600);
+            seconds_left = seconds_left % 3600;
+
+            minutes = parseInt(seconds_left / 60);
+            seconds = parseInt (seconds_left % 60);
+
+
+            // display reasult 
+            d.text(days);
+            h.text(hours);
+            m.text(minutes);
+            s.text(seconds);
+
+        }, 1000);
+    }
+}
 
 
 // 4. Init Favorite
